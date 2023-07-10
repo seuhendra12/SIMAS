3e49fae49c5e5b8344fd18a6bba8fa602747e7a4
SEUHENDRA SETIAWAN <seuhendra12@gmail.com>
2023-07-11 00:56:19 +0700

    Memperbaiki footer di dashboard-admin

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<div id="kt_app_footer" class="app-footer fixed-bottom">
+<div id="kt_app_footer" class="app-footer fixed-bottom element-below">
 	<div class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3 text-center">
 		<div class="text-dark order-2 order-md-1">
 			<span class="text-muted fw-semibold me-1 text-center">2023 &copy;</span>
