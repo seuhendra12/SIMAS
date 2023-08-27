dec3bb87dd728054d5ca71b8d5ec4b9d11897185
SEUHENDRA SETIAWAN <seuhendra12@gmail.com>
2023-08-27 20:40:23 +0700

    Memperbaiki tombol pada view kategori sampah

--- resources/views/backoffice/data-referensi/kategori-sampah/create.blade.php
+++ resources/views/backoffice/data-referensi/kategori-sampah/create.blade.php
@@ -58,21 +58,17 @@
                       <label class="d-flex align-items-center fs-6 fw-semibold mb-2">
                         <span class="required">Kategori Sampah</span>
                       </label>
-                      <input type="text" class="form-control" placeholder="Masukkan Kategori Sampah" name="name" value="{{old('name')}}" />
+                      <input type="text" class="form-control rounded-0" placeholder="Masukkan Kategori Sampah" name="name" value="{{old('name')}}" />
                     </div>
                     <div class="d-flex flex-column mb-8 fv-row">
                       <label class="d-flex align-items-center fs-6 fw-semibold mb-2">
                         <span class="required">Deskripsi</span>
                       </label>
-											<textarea name="deskripsi" id="deskripsi" cols="10" rows="10" class="form-control" placeholder="Tambahkan Deskripsi">{{ old('deskripsi') }}</textarea>
+											<textarea name="deskripsi" id="deskripsi" cols="10" rows="10" class="form-control rounded-0" placeholder="Tambahkan Deskripsi">{{ old('deskripsi') }}</textarea>
                     </div>
                     <div class="mb-10">
-                      <button type="submit" class="btn btn-primary btn-sm">
-                        <span class="indicator-label">Simpan</span>
-                        <span class="indicator-progress">Please wait...
-                          <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-                      </button>
-                      <button type="reset" class="btn btn-secondary me-5 btn-sm">Batal</button>
+                      <button type="submit" class="btn btn-primary rounded-0 fw-bold">Simpan</button>
+                      <button type="reset" class="btn btn-secondary me-5 rounded-0 fw-bold">Batal</button>
                     </div>
                   </form>
                 </div>
