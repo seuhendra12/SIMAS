40775c87c416e8e30ed2f4be24a00f6e4c90686f
Seuhendra Setiawan <seuhendra12@gmail.com>
2023-09-10 14:34:05 +0700

    Menambahkan harga sampah di seeder

--- database/seeders/JenisSampahSeeder.php
+++ database/seeders/JenisSampahSeeder.php
@@ -23,31 +23,36 @@ class JenisSampahSeeder extends Seeder
         JenisSampah::create([
             'name' => 'Tempat Pembuangan Akhir',
             'kategori_sampah_id' => $kategoriTPA->id,
-            'point_perkg' => 1
+            'point_perkg' => 1,
+            'harga_per_kg' => 0
         ]);
         
         JenisSampah::create([
             'name' => 'Sisa Makanan',
             'kategori_sampah_id' => $kategoriOrganik->id,
-            'point_perkg' => 5
+            'point_perkg' => 5,
+            'harga_per_kg' => 1000
         ]);
 
         JenisSampah::create([
             'name' => 'Kertas',
             'kategori_sampah_id' => $kategoriAnorganik->id,
-            'point_perkg' => 10
+            'point_perkg' => 10,
+            'harga_per_kg' => 2000
         ]);
 
         JenisSampah::create([
             'name' => 'Gelas Plastik',
             'kategori_sampah_id' => $kategoriAnorganik->id,
-            'point_perkg' => 15
+            'point_perkg' => 15,
+            'harga_per_kg' => 3000
         ]);
 
         JenisSampah::create([
             'name' => 'Botol Plastik',
             'kategori_sampah_id' => $kategoriAnorganik->id,
-            'point_perkg' => 20
+            'point_perkg' => 20,
+            'harga_per_kg' => 4000
         ]);
 
         // Tambahkan jenis sampah lainnya di sini dengan mengikuti pola yang sama
